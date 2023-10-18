d25a330860e4454b834216077b1a80ef05ea4d8e
duziin7 <edumotalopes1@gmail.com>
2023-10-17 21:53:36 -0300

    Aula Js e alterações

---
+++ JS/Aula02-Variaveis/main.js
@@ -0,0 +1,84 @@
+var nomeDoPrimeiroAluno = "Eduardo"  // ( ; opicional) 
+
+console.log(nomeDoPrimeiroAluno);  // imprime informação no browser
+
+let numeroAprovados = 5 // Tipo numerico
+
+console.log(numeroAprovados)
+
+console.clear() // Limpa o console do browser
+// Exemplo var
+
+var nomeAluno = "Felipe"
+nomeAluno = "Victor"
+
+console.log(nomeAluno)
+
+// Exemplo let
+
+let nomeAprendiz = "Vanessa"
+nomeAprendiz = "Luisa" // Não se pode ser redeclarado
+
+console.log(nomeAprendiz)
+
+// Exemplo const
+
+const nomeSiteWeb = "Facebook"
+// nomeSiteWeb = "Twitter" // Não se pode ser redeclarado e ter seu valor alterado
+
+console.log(nomeSiteWeb)
+console.clear()
+
+// Exibir soma de dois numeros
+
+// console.log(2 + 4)
+
+// var numero1 = 2;
+// var numero2 = 4;
+
+// var soma = numero1 + numero2;
+
+// console.log(soma)
+
+// Exibir divisao de dois numeros
+
+//console.log(2/4)
+
+// var numero1 = 4;
+// var numero2 = 2;
+
+// var soma = numero1 / numero2;
+
+// console.log(soma)
+
+var texto = "Percebemos, cada vez mais, que o fenômeno da Internet cumpre um papel essencial na formulação de alternativas às soluções ortodoxas."; // String
+
+console.log(texto)
+console.clear()
+
+// Concatenar - ato de juntar duas ou mais Strings // + So no caso de Strings 
+
+var texto1 = "No entanto, não podemos esquecer que a necessidade de renovação processual aponta para a melhoria das formas de ação."; // ou (\n) dentro das aspas
+var texto2 = "A nível organizacional, a valorização de fatores subjetivos representa uma abertura para a melhoria de alternativas às soluções ortodoxas.";
+
+var concatenar = texto1 + "\n" + texto2;
+// Escape (\n) indica um nova linha
+
+console.log(concatenar)
+// console.log(texto1 + texto2)
+console.clear()
+
+
+// Valores booleanos / TRUE OU FALSE - VERDADEIRO OU FALSO
+
+var osDeVerdadeAgenteSabeQuemE = true;
+
+// Operadores 
+
+console.log(8 * null)
+console.log("5" - 1)
+console.log("5" + 1) // ou console.log(1 + "5") mesmo resultado - Concatenação pois existe uma String
+console.log("five" * 2)
+console.clear()
+
+// Visualizar slide 22 sobre Hierarquia
