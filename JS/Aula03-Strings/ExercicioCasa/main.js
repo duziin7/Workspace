1c0e8a0f7292f0a4433810ebb70b8fc90a00b1f2
duziin7 <edumotalopes1@gmail.com>
2023-10-22 23:57:18 -0300

    Aula03

---
+++ JS/Aula03-Strings/ExercicioCasa/main.js
@@ -0,0 +1,6 @@
+let devFuture = 'Estamos aprendendo JavaScript e seremos futuros Devs';
+
+console.log('Posição: ' + devFuture.charAt(19));
+console.log('Tamanho: ' + devFuture.length);
+console.log('Maiúsculo: ' + devFuture.toUpperCase());
+console.log('Palavra: ' + devFuture.substring(48));
