20c34ec59d25b8b0d5f4f55a0f74d47d17515616
duziin7 <edumotalopes1@gmail.com>
2023-11-17 20:25:13 -0300

    React 02

--- React/primeiro-projeto-react/src/components/Exemplo.jsx
+++ React/primeiro-projeto-react/src/components/Exemplo.jsx
@@ -1,5 +1,20 @@
 import React from "react"; 
 
-const Exemplo = () => <h1>Dificil de aprender React</h1>;
+let estilo = {listStyle: 'none', color: 'pink', fontSize: '50px', textAling: 'center'};
 
-export default Exemplo;
+const Pipoca = () => {
+    return (
+        <>
+            <h1>Sexta-Feira depressiva</h1>
+            <p>Vão Reprovar</p>
+            <ul style={estilo}>     {/* <ul style={{listStyle: "none"}}> */}
+                <li>Maria Eduarda</li>
+                <li>Anna Clara</li>
+                <li>Todos</li>
+                <li>Menos eu</li>
+            </ul>
+        </>
+    );
+}
+
+export default Pipoca; // Libera para outros arquivos visualizarem
